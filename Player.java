200baee32daada30285f61199e9d3d31ab46da11
Joseph Jiang <josephjiang@berkeley.edu>
2015-11-19 23:33:04 -0800

    Player.java created (constructor incomplete)

---
+++ Player.java
@@ -0,0 +1,67 @@
+public class Player {
+    /** Properties owned by this Player */
+    private ArrayList<BoardPiece> _properties;
+    
+    /** Location of the player */
+    private BoardNode _location;
+
+    /** ID of the player */
+    private int _id;
+
+    /** Money the player own */
+    private int _money;
+    
+    /** Has "get out of jail free" */
+    private boolean _jailFree;
+
+    //Constructor Here
+ 
+
+
+
+
+    /** Returns properties owned */
+    public ArrayList<BoardPiece> properties() {
+        return _properties;
+    }
+    
+    /** Returns location of player */
+    public BoardNode location() {
+        return _location;
+    }
+
+    /** Returns players number ID */
+    public int getID() {
+        return _id;
+    }
+
+    /** Returns the amount of money the player has */
+    public int money() {
+        return _money;
+    }
+
+    /** Returns whether the player has a "get out of jail free" */
+    public boolean jailFree() {
+        return _jailFree;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
